4fb6a612513dbfec2f439e295e97fb523d42b528
Ramon Velarde <rvelarde83@hotmail.com>
2022-11-02 00:03:51 -0700

    working chat, bad words function pending

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Super chat app
+bad words functions not deployed "firebase deploy --only functions"error
--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 import firebase from 'firebase/compat/app'; 
      import 'firebase/compat/firestore';
      import 'firebase/compat/auth';
-import 'firebase/analytics';
+
+import 'firebase/compat/analytics'
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
@@ -22,52 +23,61 @@ firebase.initializeApp({
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
+const analytics = firebase.analytics();
 
 
 function App() {
+
   const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-       
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
       </header>
+
       <section>
         {user ? <ChatRoom /> : <SignIn />}
-
       </section>
+
     </div>
   );
 }
 
-function SignIn(){
-  const signInWithGoogle = () =>{
+function SignIn() {
+
+  const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
   }
 
-
-  return(
-    <button onClick={signInWithGoogle}>Sign in with Google</button>
-
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
   )
+
 }
 
-function SignOut(){
+function SignOut() {
   return auth.currentUser && (
-    <button onClick={() => auth.signOut()}>Sign Out</button>
-
-   )
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
 }
 
 
 function ChatRoom() {
-
+  const dummy = useRef();
   const messagesRef = firestore.collection('messages');
   const query = messagesRef.orderBy('createdAt').limit(25);
-  const [messages] = useCollectionData(query, { idField: 'id' });
-  const[formValue, setFormValue] = useState('');
 
-  
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
   const sendMessage = async (e) => {
     e.preventDefault();
 
@@ -79,34 +89,43 @@ function ChatRoom() {
       uid,
       photoURL
     })
-  return(
-    <>
-    <div>
-      {messages && messages.map(msg=> <ChatMessage key={msg.id} message={msg}/>)}
-    </div>
 
-    <form>
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
-      <button type="submit"> Submit </button>
-    <div>
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
 
-    </div>
-    </>
-  )
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>CHAT!</button>
+
+    </form>
+  </>)
 }
 
+
 function ChatMessage(props) {
-  const{ text, uid, photoURL} =props.message;
+  const { text, uid, photoURL } = props.message;
 
-  return(
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
     <div className={`message ${messageClass}`}>
-      <img src={photoURL} />
-      <p> {text}</p>
-
-      </div>
-  )
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
 }
 
 
 export default App;
- 
